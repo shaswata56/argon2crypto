66a98c45e6c502f2dbd98387b04c0aeeee8ebba3
shaswata56 <shaswata56@gmail.com>
2019-10-18 03:37:39 +0600

    added package for strong password encryption

---
+++ argon2crypto.go
@@ -0,0 +1,36 @@
+package argon2crypto
+
+import (
+	"crypto/rand"
+	"encoding/base64"
+	"golang.org/x/crypto/argon2"
+)
+
+type params struct {
+	memory	    uint32
+	iterations  uint32
+	parallelism uint8
+	saltLength  uint32
+	keyLength   uint32
+}
+
+func getHashedPassword(password string, p *params) (encodedHash string, err error) {
+	salt, err := generateRandomBytes(p.saltLength)
+	if err != nil { return "", err }
+	hash := argon2.IDKey([]byte(password), salt, p.iterations, p.memory, p.parallelism, p.keyLength)
+	b64salt := base64.RawStdEncoding.EncodeToString(salt)
+	b64hash := base64.RawStdEncoding.EncodeToString(hash)
+	encodedHash = string(p.memory) + string(p.iterations) + string(p.parallelism) + b64salt + b64hash
+	return encodedHash, nil
+}
+
+func generateRandomBytes(n uint32) ([]byte, error) {
+	rawBytes := make([]byte, n)
+	_, err := rand.Read(rawBytes)
+	if err != nil { return  nil, err }
+	return rawBytes, nil
+}
+
+func check(stored, received string) bool {
+	return stored == received
+}
