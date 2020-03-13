b47812b17efe3b1ffc32335fc40db60cde5ed774
shaswata56 <shaswata56@gmail.com>
2020-03-13 13:14:10 +0600

    fixed a typo

--- argon2crypto.go
+++ argon2crypto.go
@@ -6,31 +6,36 @@ import (
 	"golang.org/x/crypto/argon2"
 )
 
-type params struct {
-	memory	    uint32
-	iterations  uint32
-	parallelism uint8
-	saltLength  uint32
-	keyLength   uint32
+type Params struct {
+	Memory      uint32
+	Iterations  uint32
+	Parallelism uint8
+	SaltLength  uint32
+	KeyLength   uint32
 }
 
-func GetHashedPassword(password string, p *params) (encodedHash string, err error) {
-	salt, err := generateRandomBytes(p.saltLength)
-	if err != nil { return "", err }
-	hash := argon2.IDKey([]byte(password), salt, p.iterations, p.memory, p.parallelism, p.keyLength)
+func GetHashedPassword(password string, p Params) (encodedHash string, salt []byte, err error) {
+	salt, err = GenerateRandomBytes(p.SaltLength)
+	if err != nil { return "", []byte(""), err }
+	hash := argon2.IDKey([]byte(password), salt, p.Iterations, p.Memory, p.Parallelism, p.KeyLength)
 	b64salt := base64.RawStdEncoding.EncodeToString(salt)
 	b64hash := base64.RawStdEncoding.EncodeToString(hash)
-	encodedHash = string(p.memory) + string(p.iterations) + string(p.parallelism) + b64salt + b64hash
-	return encodedHash, nil
+	encodedHash = string(p.Memory) + b64salt + string(p.Iterations) + b64hash + string(p.Parallelism)
+	return encodedHash, salt,nil
 }
 
-func generateRandomBytes(n uint32) ([]byte, error) {
+func GenerateRandomBytes(n uint32) ([]byte, error) {
 	rawBytes := make([]byte, n)
 	_, err := rand.Read(rawBytes)
 	if err != nil { return  nil, err }
 	return rawBytes, nil
 }
 
-func Check(stored, received string) bool {
-	return stored == received
+func Check(dbPassword string, dbSalt []byte, received string, p Params) bool {
+	hash := argon2.IDKey([]byte(received), dbSalt, p.Iterations, p.Memory, p.Parallelism, p.KeyLength)
+	b64salt := base64.RawStdEncoding.EncodeToString(dbSalt)
+	b64hash := base64.RawStdEncoding.EncodeToString(hash)
+	encodedHash := string(p.Memory) + b64salt + string(p.Iterations) + b64hash + string(p.Parallelism)
+	if encodedHash == dbPassword { return true }
+	return false
 }
